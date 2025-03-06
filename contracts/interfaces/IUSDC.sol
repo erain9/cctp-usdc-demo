e13b01794c06269aa60c4d21273ce8058d859b6e
erain9 <yiyu@axelar.network>
2025-03-05 23:26:28 -0500

    Fix: Add missing IUSDC interface definition

--- contracts/interfaces/IUSDC.sol
+++ contracts/interfaces/IUSDC.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+/**
+ * @title IUSDC
+ * @dev Interface for USDC token with basic ERC20 functions
+ */
+interface IUSDC {
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
